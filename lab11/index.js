1b2d48d8d23fc0272053702c1e1a6411531b0ada
focitti <focitt01@lab116t.eecs.tufts.edu>
2017-06-28 13:27:56 -0400

    add /lab8 path take two

--- lab11/index.js
+++ lab11/index.js
@@ -10,7 +10,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/lab8', function(request, response) {
-  response.render('pages/index');
+  response.render('public/index');
 });
 
 app.listen(app.get('port'), function() {
