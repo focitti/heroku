1b9d19e08b8bfddedd831240fca68084693464cb
focitti <focitt01@lab116t.eecs.tufts.edu>
2017-06-29 00:12:34 -0400

    now will you work?

--- lab12/index.js
+++ lab12/index.js
@@ -5,6 +5,14 @@ var express = require('express');
 var app = express();
 var request = require('request');
 
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
 request({
     url: 'http://developer.mbta.com/lib/rthr/red.json',
     headers: {
@@ -20,14 +28,6 @@ request({
 	});
     }
 });
-
-app.use(express.static(__dirname));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.set('port', (process.env.PORT || 5000));
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
